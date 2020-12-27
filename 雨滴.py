7d3c634d2404c215d48c33d3328849a487804bc4
weiyinfu <weiyinfu.weiyinfu@bytedance.com>
2020-12-27 22:16:02 +0800

    思维是地球上最美丽的花朵

--- 雨滴.py
+++ 雨滴.py
@@ -4,32 +4,40 @@ from tqdm import tqdm
 
 """
 长为1m的路，半径为1cm的雨滴，雨滴在路上随机分布，问多少个雨滴才能把路淋湿？
-答案应该在725附近
+答案应该在725附近。
+
+关于这个问题，某剑桥大学数学博士给出了一种解法，见文档road_problem.
 """
 
 
-def model_solve(n, m, ratio=10):
-    # 路的长度为n，雨滴宽度为m,ratio表示一个雨滴的离散化长度
-    n = n / (m / ratio)
-    n = int(n)
-    m = int(ratio)
-    a = np.zeros(n, dtype=np.bool)
-    drop = 0
-    while np.count_nonzero(a) < n:
-        x = np.random.randint(0, n)
-        beg = x - m // 2
-        end = x + m // 2
-        end = min(n, end)
-        beg = max(0, beg)
-        a[beg:end] = True
-        drop += 1
-    return drop
+def model_solve(n, m, ratio=10, times=1000):
+    """
+    使用蒙特卡洛方法求解此问题
+    :param n:
+    :param m:
+    :param ratio:
+    :return:
+    """
 
+    def go():
+        # 路的长度为n，雨滴宽度为m,ratio表示一个雨滴的离散化长度
+        nonlocal n, m
+        n = n / (m / ratio)
+        n = int(n)
+        m = int(ratio)
+        a = np.zeros(n, dtype=np.bool)
+        drop = 0
+        while np.count_nonzero(a) < n:
+            x = np.random.randint(0, n)
+            beg = x - m // 2
+            end = x + m // 2
+            end = min(n, end)
+            beg = max(0, beg)
+            a[beg:end] = True
+            drop += 1
+        return drop
 
-def bruteforce_manytimes(n, m, ratio, times):
-    ans = []
-    for i in tqdm(range(times)):
-        ans.append(model_solve(n, m, ratio))
+    ans = [go() for _ in tqdm(range(times))]
     mu = np.mean(ans)
     sigma = np.std(ans)
     print(f"mu={mu} sigma={sigma}")
@@ -38,4 +46,4 @@ def bruteforce_manytimes(n, m, ratio, times):
     return mu
 
 
-print(bruteforce_manytimes(1, 0.01, ratio=100, times=1000))
+print(model_solve(1, 0.01, ratio=100, times=1000))
